a46b23b5ad72dddc155480d9e0e2f61b08a5b9a5
chahana7 <chahamal5@gmail.com>
2024-11-24 17:42:31 -0800

    Update test_Dog.py

--- test_Dog.py
+++ test_Dog.py
@@ -1,8 +1,9 @@
-from Dog import Dog 
+from Dog import Dog  # Import the Dog class
+
 # Create an instance of the Dog class
 my_dog = Dog("Buddy", 3, "Golden Retriever")
 
 # Test the methods
-print(my_dog.make_sound()) 
-print(my_dog.move())        
-print(my_dog.fetch())      
+print(my_dog.make_sound())  # Output: Woof!
+print(my_dog.move())        # Output: The dog runs around happily.
+print(my_dog.fetch())       # Output: Buddy fetches the ball eagerly.
