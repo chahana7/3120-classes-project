e327d266c571f7723222a3b9426c9b2da8919b4f
chahana7 <chahamal5@gmail.com>
2024-11-24 17:22:37 -0800

    Update Dog.py

--- Dog.py
+++ Dog.py
@@ -2,7 +2,7 @@ from Animal import Animal
 
 class Dog(Animal):
     def __init__(self, name, age, breed):
-        super().__init__(name, age)  # Call the parent class constructor
+        super().__init__(name, age) 
         self.breed = breed
 
     def make_sound(self):
