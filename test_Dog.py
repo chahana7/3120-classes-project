1bbf7082bacff746b9dbc7bc396a7ecc2fa89ad0
chahana7 <chahamal5@gmail.com>
2024-11-24 17:48:28 -0800

    Delete test_Dog.py

--- test_Dog.py
+++
@@ -1,9 +0,0 @@
-from Dog import Dog  # Import the Dog class
-
-# Create an instance of the Dog class
-my_dog = Dog("Buddy", 3, "Golden Retriever")
-
-# Test the methods
-print(my_dog.make_sound())  # Output: Woof!
-print(my_dog.move())        # Output: The dog runs around happily.
-print(my_dog.fetch())       # Output: Buddy fetches the ball eagerly.
