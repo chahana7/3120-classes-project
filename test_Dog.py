e9194f5094da9be83fd869f3a623af95364d992c
chahana7 <chahamal5@gmail.com>
2024-11-24 17:41:30 -0800

    Create test_Dog.py

---
+++ test_Dog.py
@@ -0,0 +1,8 @@
+from Dog import Dog 
+# Create an instance of the Dog class
+my_dog = Dog("Buddy", 3, "Golden Retriever")
+
+# Test the methods
+print(my_dog.make_sound()) 
+print(my_dog.move())        
+print(my_dog.fetch())      
